d0028bf1e3f3804024e45be91a98a573c3950ea8
duaa-braik <duaa13braik@gmail.com>
2023-03-11 23:02:25 +0200

    Added DbContext class

---
+++ Conduit.Infrastructure/ConduitDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Conduit.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Conduit.Infrastructure
+{
+    public class ConduitDbContext : DbContext
+    {
+        public DbSet<Article> Article { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Tag> Tag { get; set; }
+        public DbSet<Follow> Follow { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = Conduit");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Follow>()
+                .HasKey(k => new { k.FollowerId, k.FolloweeId });
+
+            modelBuilder.Entity<Follow>()
+                .HasOne(u => u.Followee)
+                .WithMany(u => u.Followers)
+                .HasForeignKey(u => u.FolloweeId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Follow>()
+                .HasOne(u => u.Follower)
+                .WithMany(u => u.Followings)
+                .HasForeignKey(u => u.FollowerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Article>()
+                .HasOne(a => a.User)
+                .WithMany(u => u.Articles);
+        }
+    }
+}
