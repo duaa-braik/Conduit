e533caaaccb2452319c33ef86d5ca2f649d5a0b9
duaa-braik <duaa13braik@gmail.com>
2023-03-26 17:17:17 +0200

    Added Generic Repository

---
+++ Conduit.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using Conduit.Domain.Interfaces;
+
+namespace Conduit.Infrastructure.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        public Task<T> CreateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
