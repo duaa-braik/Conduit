bab799072bdf1b2eb55c2b55698758301c7c1ad1
duaa-braik <duaa13braik@gmail.com>
2023-03-27 14:25:35 +0200

    Added user service

---
+++ Conduit.Application/Services/UserService.cs
@@ -0,0 +1,14 @@
+﻿using Conduit.Domain.Interfaces;
+
+namespace Conduit.Application.Services
+{
+    public class UserService
+    {
+        private readonly IUserRpository userRepository;
+
+        public UserService(IUserRpository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+    }
+}
