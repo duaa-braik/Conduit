35c667230d417a444b1dd73b12f470b9a319e40e
duaa-braik <duaa13braik@gmail.com>
2023-03-27 13:59:20 +0200

    Added token generator service

---
+++ Conduit.API/Authentication/Service/TokenGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Conduit.Domain.DTOs;
+
+namespace Conduit.API.Authentication.Service
+{
+    public class TokenGenerator
+    {
+        public string Generate(UserDto userModel, JwtSettings jwtSettings)
+        {
+
+        }
+    }
+}
