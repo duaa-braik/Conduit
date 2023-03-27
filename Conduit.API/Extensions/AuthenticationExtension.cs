22ac7e4dc98278f93cf478f07a6dd9842037e420
duaa-braik <duaa13braik@gmail.com>
2023-03-27 13:49:12 +0200

    Added extension method implementation

--- Conduit.API/Extensions/AuthenticationExtension.cs
+++ Conduit.API/Extensions/AuthenticationExtension.cs
@@ -1,11 +1,33 @@
-﻿namespace Conduit.API.Extensions
+﻿using Conduit.API.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Conduit.API.Extensions
 {
     public static class AuthenticationExtension
     {
         public static IServiceCollection AddAuth
             (this IServiceCollection services, ConfigurationManager configuration)
         {
+            var jwtSettings = new JwtSettings();
+            configuration.Bind("JwtSettings", jwtSettings);
 
+            services.AddSingleton(Options.Create(jwtSettings));
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(
+                options => options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(jwtSettings.Secret)),
+                });
+            return services;
         }
     }
 }
