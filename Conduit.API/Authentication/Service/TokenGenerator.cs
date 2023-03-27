5f894972e5203f175bfe052077f0682af72226ed
duaa-braik <duaa13braik@gmail.com>
2023-03-27 14:10:02 +0200

    Added Implementation for generate method

--- Conduit.API/Authentication/Service/TokenGenerator.cs
+++ Conduit.API/Authentication/Service/TokenGenerator.cs
@@ -1,12 +1,34 @@
 ﻿using Conduit.Domain.DTOs;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Conduit.API.Authentication.Service
 {
-    public class TokenGenerator
+    public class TokenGenerator : ITokenGenerator
     {
         public string Generate(UserDto userModel, JwtSettings jwtSettings)
         {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                new Claim("Email", userModel.Email),
+                new Claim("UserName", userModel.UserName)
+            };
+
+            var token = new JwtSecurityToken(
+                jwtSettings.Issuer,
+                jwtSettings.Audience,
+                claims,
+                signingCredentials: credentials,
+                expires: DateTime.UtcNow.AddDays(1));
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
