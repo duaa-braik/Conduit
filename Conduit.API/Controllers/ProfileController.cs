a9378d124d2580ccaf0acf5efd12690dc456382b
duaa-braik <duaa13braik@gmail.com>
2023-03-28 20:18:22 +0200

    Added Profile Controller

---
+++ Conduit.API/Controllers/ProfileController.cs
@@ -0,0 +1,18 @@
+﻿using Conduit.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Conduit.API.Controllers
+{
+    [ApiController]
+    [Route("/api/profiles")]
+    public class ProfileController : ControllerBase
+    {
+        private readonly IProfileService profileService;
+
+        public ProfileController(IProfileService profileService)
+        {
+            this.profileService = profileService;
+        }
+    }
+}
