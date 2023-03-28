1198fa3a4ce23397983941e9a341688ff6fe63b9
duaa-braik <duaa13braik@gmail.com>
2023-03-28 23:01:04 +0200

    Applied some refactoring to GetUserProfileMethod

--- Conduit.Application/Services/ProfileService.cs
+++ Conduit.Application/Services/ProfileService.cs
@@ -3,8 +3,6 @@ using Conduit.Application.Interfaces;
 using Conduit.Domain.DTOs;
 using Conduit.Domain.Entities;
 using Conduit.Domain.Interfaces;
-using Conduit.Domain.Profiles;
-using System.Runtime.InteropServices;
 
 namespace Conduit.Application.Services
 {
@@ -12,6 +10,13 @@ namespace Conduit.Application.Services
     {
         private readonly IUserRepository userRepository;
         private readonly IMapper mapper;
+
+        private class UsersRelationship
+        {
+            public User Follower { get; set; }
+            public User Followee { get; set; }
+            public bool IsFollowing { get; set; }
+        }
 
         public ProfileService(IUserRepository userRepository, IMapper mapper)
         {
@@ -28,17 +33,33 @@ namespace Conduit.Application.Services
 
         public async Task<UserProfileDto> GetUserProfile(string Username, string CurrentUserName)
         {
-            var User = await userRepository.GetUserByUsername(Username);
+            var Users = await GetUsers(Username, CurrentUserName);
 
-            var CurrentUser = await userRepository.GetUserWithFollowings(CurrentUserName);
+            var User = Users.Followee;
 
-            bool isFollowing = CheckFollowStatus(User.UserId, CurrentUser);
+            bool isFollowing = Users.IsFollowing;
 
             var UserProfile = mapper.Map<UserProfileDto>(User);
 
             UserProfile.Following = isFollowing;
 
             return UserProfile;
+        }
+
+        private async Task<UsersRelationship> GetUsers(string FolloweeName, string FollowerName)
+        {
+            var User = await userRepository.GetUserByUsername(FolloweeName);
+
+            var CurrentUser = await userRepository.GetUserWithFollowings(FollowerName);
+
+            bool isFollowing = CheckFollowStatus(User.UserId, CurrentUser);
+
+            return new UsersRelationship
+            {
+                Followee = User,
+                Follower = CurrentUser,
+                IsFollowing = isFollowing
+            };
         }
 
         private bool CheckFollowStatus(int userId, User currentUser)
