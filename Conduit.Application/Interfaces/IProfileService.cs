9a11c40dbe0112fc4ccfeb8bad7d7578fa9304a9
duaa-braik <duaa13braik@gmail.com>
2023-03-28 20:05:33 +0200

    Added a method to check follow status between 2 users

--- Conduit.Application/Interfaces/IProfileService.cs
+++ Conduit.Application/Interfaces/IProfileService.cs
@@ -1,9 +1,11 @@
 ﻿using Conduit.Domain.DTOs;
+using System.Runtime.InteropServices;
 
 namespace Conduit.Application.Interfaces
 {
     public interface IProfileService
     {
+        Task<UserProfileDto> GetUserProfile(string Username);
         Task<UserProfileDto> GetUserProfile(string Username, string CurrentUserName);
     }
 }
--- Conduit.Application/Services/ProfileService.cs
+++ Conduit.Application/Services/ProfileService.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
 using Conduit.Application.Interfaces;
 using Conduit.Domain.DTOs;
+using Conduit.Domain.Entities;
 using Conduit.Domain.Interfaces;
+using Conduit.Domain.Profiles;
+using System.Runtime.InteropServices;
 
 namespace Conduit.Application.Services
 {
@@ -16,15 +19,31 @@ namespace Conduit.Application.Services
             this.mapper = mapper;
         }
 
+        public async Task<UserProfileDto> GetUserProfile(string Username)
+        {
+            var User = await userRepository.GetUserByUsername(Username);
+
+            return mapper.Map<UserProfileDto>(User);
+        }
+
         public async Task<UserProfileDto> GetUserProfile(string Username, string CurrentUserName)
         {
             var User = await userRepository.GetUserByUsername(Username);
 
             var CurrentUser = await userRepository.GetUserWithFollowings(CurrentUserName);
 
+            bool isFollowing = CheckFollowStatus(User.UserId, CurrentUser);
+
             var UserProfile = mapper.Map<UserProfileDto>(User);
 
-            return mapper.Map<UserProfileDto>(User);
+            UserProfile.Following = isFollowing;
+
+            return UserProfile;
+        }
+
+        private bool CheckFollowStatus(int userId, User currentUser)
+        {
+            return currentUser.Followings.Any(user => user.FolloweeId == userId);
         }
     }
 }
