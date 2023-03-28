3d02110c101777ae226a0223cdb595d94b257c00
duaa-braik <duaa13braik@gmail.com>
2023-03-28 19:35:53 +0200

    Added ProfileService class

---
+++ Conduit.Application/Services/ProfileService.cs
@@ -0,0 +1,15 @@
+﻿using Conduit.Application.Interfaces;
+using Conduit.Domain.Interfaces;
+
+namespace Conduit.Application.Services
+{
+    public class ProfileService : IProfileService
+    {
+        private readonly IUserRepository userRepository;
+
+        public ProfileService(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+    }
+}
