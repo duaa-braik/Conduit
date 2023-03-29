9e3b5106c7a45797e82d799f951dc16a05bd857c
duaa-braik <duaa13braik@gmail.com>
2023-03-29 19:46:59 +0200

    Added object mapping for article class

--- Conduit.Application/Services/ArticleService.cs
+++ Conduit.Application/Services/ArticleService.cs
@@ -1,5 +1,7 @@
-﻿using Conduit.Application.Interfaces;
+﻿using AutoMapper;
+using Conduit.Application.Interfaces;
 using Conduit.Domain.DTOs;
+using Conduit.Domain.Entities;
 using Conduit.Domain.Interfaces;
 
 namespace Conduit.Application.Services
@@ -8,16 +10,19 @@ namespace Conduit.Application.Services
     {
         private readonly IArticleRepository articleRepository;
         private readonly IUserRepository userRepository;
+        private readonly IMapper mapper;
 
-        public ArticleService(IArticleRepository articleRepository, IUserRepository userRepository)
+        public ArticleService
+            (IArticleRepository articleRepository, IUserRepository userRepository, IMapper mapper)
         {
             this.articleRepository = articleRepository;
             this.userRepository = userRepository;
+            this.mapper = mapper;
         }
 
         public async Task<ArticleDto> AddArticle(ArticleCreationDto articleDetails, string Username)
         {
-
+            
         }
     }
 }
