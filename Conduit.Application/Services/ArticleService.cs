79b91e0768be024948efefed793fbb4d41d3c012
duaa-braik <duaa13braik@gmail.com>
2023-03-29 19:41:04 +0200

    Added Article Dtos

--- Conduit.Application/Services/ArticleService.cs
+++ Conduit.Application/Services/ArticleService.cs
@@ -1,4 +1,5 @@
 ﻿using Conduit.Application.Interfaces;
+using Conduit.Domain.DTOs;
 using Conduit.Domain.Interfaces;
 
 namespace Conduit.Application.Services
@@ -6,10 +7,17 @@ namespace Conduit.Application.Services
     public class ArticleService : IArticleService
     {
         private readonly IArticleRepository articleRepository;
+        private readonly IUserRepository userRepository;
 
-        public ArticleService(IArticleRepository articleRepository)
+        public ArticleService(IArticleRepository articleRepository, IUserRepository userRepository)
         {
             this.articleRepository = articleRepository;
+            this.userRepository = userRepository;
+        }
+
+        public async Task<ArticleDto> AddArticle(ArticleCreationDto articleDetails, string Username)
+        {
+
         }
     }
 }
